55337fc2526603c632c7e979d4487d4a5251a181
David Conner <robotics@cnu.edu>
2023-08-01 17:22:08 -0400

    update for change in yaml loading for Iron messages; subscriber state test still not functional

--- flexbe_states/tests/pub.launch.py
+++ flexbe_states/tests/pub.launch.py
@@ -40,5 +40,5 @@ from launch.actions import ExecuteProcess
 
 def generate_launch_description():
     return LaunchDescription([
-        ExecuteProcess(cmd=['ros2', 'topic', 'pub', '/test', 'geometry_msgs/Pose', 'position: {x: 6.4}'])
+        ExecuteProcess(cmd=['ros2', 'topic', 'pub', '/test', 'geometry_msgs/Pose', '{position: {x: 6.4}}'])
     ])
