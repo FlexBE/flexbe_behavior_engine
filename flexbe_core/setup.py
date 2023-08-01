dd75c7f684e076577f5a85585cddd065f8b76a62
David Conner <robotics@cnu.edu>
2023-08-01 11:45:00 -0400

    2.3.2

--- flexbe_core/setup.py
+++ flexbe_core/setup.py
@@ -6,7 +6,7 @@ PACKAGE_NAME = 'flexbe_core'
 
 setup(
     name=PACKAGE_NAME,
-    version='2.3.1',
+    version='2.3.2',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + PACKAGE_NAME]),
