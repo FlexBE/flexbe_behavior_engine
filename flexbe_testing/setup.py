40e6c74a6e82044a62379225347a4432c354af70
David Conner <robotics@cnu.edu>
2026-03-23 01:52:56 -0400

    bump version to 4.1.0

--- flexbe_testing/setup.py
+++ flexbe_testing/setup.py
@@ -8,7 +8,7 @@ package_name = 'flexbe_testing'
 
 setup(
     name=package_name,
-    version='4.0.3',
+    version='4.1.0',
     packages=[package_name],
     data_files=[
         (os.path.join('share', package_name), glob('launch/*.launch.py')),
